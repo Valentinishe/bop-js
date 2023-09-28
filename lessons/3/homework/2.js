1c7a762354e77c2683ab196443fb128317d00623
Valentyn <valentynishe.gmail.com>
2023-09-28 16:34:13 +0300

    Koval VP Homework 3/2

--- lessons/3/homework/2.js
+++ lessons/3/homework/2.js
@@ -1,3 +1,10 @@
 // Використовуючи конструкцію if..else, напишіть код, що реагуе на значення змінної let age;
 // Якщо age менще 18 - виведіть у консоль "Access denied!", в іншому випадку — Welcome!”
 
+const age = 20;
+
+if(age < 18) {
+    console.log('Access denied!');
+} else {
+    console.log('Welcome!');
+}
