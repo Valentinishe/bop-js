d02018ce08d691da0a5fd0a685a7a0daf90e5294
Valentyn <valentynishe.gmail.com>
2023-09-28 16:27:35 +0300

    uploaded test lessons

---
+++ lessons/2/homework/1.js
@@ -0,0 +1,21 @@
+// Опишіть доклано очікуваний результат (та чому саме так) виконання наступних прикладів коду:
+console.log(1);
+
+console.log("1");
+
+console.log(true);
+
+console.log(null);
+
+console.log(undefined);
+
+console.log(!!"");
+
+console.log(!!1);
+
+console.log(+"1");
+
+console.log(!!NaN);
+
+console.log("1" + 1);
+
